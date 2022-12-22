2f5d1f970d496646e3ad88669569b8d81da19750
Yuchen Liu <101923999+alexliu07@users.noreply.github.com>
2022-12-22 17:55:52 +0800

    Add Prime Number Judge Change Header File

--- main.cpp
+++ main.cpp
@@ -2,12 +2,24 @@
 // Created by liuyu on 2022/12/22.
 //
 
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
+bool isPrime(int n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int simp(int n) {
     int result = 1;
+    if (isPrime(n)) {
+        return result;
+    }
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             if ((n / i) % i == 0) {
