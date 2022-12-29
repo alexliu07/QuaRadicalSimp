81535a2b12672d3c525789ca73183490dd960784
Yuchen Liu <101923999+alexliu07@users.noreply.github.com>
2022-12-29 09:17:31 +0800

    Added the simplification of the radical of the custom number of times

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-# 二次根式化简工具<br>Quadratic Radical Simplification Tool
- 一个化简二次根式的程序<br>A Program to Simplify Quadratic Radicals
+# 根式化简工具<br>Radical Simplification Tool
+
+一个化简根式的程序
+
+A Program to Simplify Radicals
+
 ### 用法 Usage
+
 ***
-编译main.cpp并运行<br>Compile main.cpp and run
+编译main.cpp并运行
+
+Compile main.cpp and run
+
 #### 输入 Input
-输入共一行，输入待化简的二次根式里面的数字n。<br>Enter a total of one line, enter the number n in the quadratic radical to be simplified.<br>
-ex. n=2700<br>`>sqrt:2700`
+
+输入共两行，一行一个整数，第一行输入待化简的根式里面的数字n，第二行输入根式的次数。
+
+Input a total of two lines, one integer per line, enter the number n in the radical to be simplified in the first line,
+and enter the times of radicals in the second line .
+
+ex. n=2700,times=2
+
+`>sqrt:2700`
+
+`>times:2`
+
 #### 输出 Output
-输出共一行，前一个数字为二次根式的系数，后一个数字为化简后二次根式里面的数字。<br>Output a total of one line, the first number is the coefficient of the quadratic radical, and the latter number is the number in the quadratic radical after simplification.<br>
-ex. 30√3<br>`>30 sqrt(3)`
+
+输出共一行，前一个数字为根式的系数，"sqrt"后面的数字为根式的次数，括号里面的数字为化简后根式里面的数字。
+
+Output a total of one line, the first number is the coefficient of the radical, the number after 'sqrt' is the number of
+the radical, and the number in the brackets is the number in the radical after simplification.
+
+ex. 30√3
+
+`>30 sqrt2(3)`
--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@
 
 using namespace std;
 
+bool isEvenly(int a, int b, int times) {
+    for (int i = 0; i < times; i++) {
+        if (a % b != 0) {
+            return false;
+        }
+        a /= b;
+    }
+    return true;
+}
+
 bool isPrime(int n) {
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
@@ -15,35 +25,35 @@ bool isPrime(int n) {
     return true;
 }
 
-int simp(int n) {
+int simp(int n, int times) {
     int result = 1;
     if (isPrime(n)) {
         return result;
     }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            if ((n / i) % i == 0) {
-                result *= i;
-                n = n / i / i;
-                result *= simp(n);
-                break;
-            }
+    for (int i = 2; pow(i, times) <= n; i++) {
+        if (isEvenly(n, i, times)) {
+            result *= i;
+            n = n / pow(i, times);
+            result *= simp(n, times);
+            break;
         }
     }
     return result;
 }
 
 int main() {
-    int ans, n, x = 1;
+    int ans, n, x = 1, times;
     cout << "sqrt:";
     cin >> n;
-    ans = simp(n);
-    if (ans * ans == n) {
+    cout << "times:";
+    cin >> times;
+    ans = simp(n, times);
+    if (pow(ans, times) == n) {
         cout << ans;
     } else if (ans == 1) {
-        cout << "sqrt(" << n << ")";
+        cout << "sqrt" << times << "(" << n << ")";
     } else {
-        cout << ans << " sqrt(" << (n / ans / ans) << ")";
+        cout << ans << " sqrt" << times << "(" << (n / pow(ans, times)) << ")";
     }
     return 0;
 }
